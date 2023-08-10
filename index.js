079e226aae950039de1f474b617d6e473683c14f
Rahmat Ardiansyah <rahmatardiansyah147@gmail.com>
2023-08-10 21:53:14 +0700

    menambahakan hak akses read folder images

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ const fileFilter = (req, file, cb) => {
 app.use(
   multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
 );
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
